d1a9975104b35b340a66a4e39e0fbbb2fcf220c3
Christian <Christian@Mac-mini>
2014-02-23 14:52:39 +0100

    Implemented functionality to handle GPIO access

--- gpio.c
+++ gpio.c
@@ -21,3 +21,62 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
  * MA 02110-1301, USA.
  */
+
+#include "gpio.h"
+
+volatile unsigned *gpio;	// I/O access
+
+/**
+ * Set GPIO high
+ *
+ * @param g GPIO
+ */
+void gpio_set(unsigned int g) {
+	GPIO_SET = g;
+}
+
+/**
+ * Set GPIO low
+ *
+ * @param g GPIO
+ */
+void gpio_clear(unsigned int g) {
+	GPIO_CLR = g;
+}
+
+/**
+ * Set GPIO as input
+ *
+ * @param g GPIO
+ */
+void gpio_input(unsigned int g) {
+	INP_GPIO(g);
+}
+
+/**
+ * Set GPIO as output
+ *
+ * @param g GPIO
+ */
+void gpio_output(unsigned int g) {
+	OUT_GPIO(g);
+}
+
+/**
+ * Read status of GPIO
+ *
+ * @param g GPIO
+ * @return Status of GPIO
+ */
+unsigned char gpio_read(unsigned int g) {
+	return g & ~(*(gpio + 13));
+}
+
+/**
+ * Read status of all GPIOs
+ *
+ * @return Status of all GPIOs
+ */
+unsigned int gpio_read_all() {
+	return ~(*(gpio + 13));;
+}
--- gpio.h
+++ gpio.h
@@ -21,3 +21,17 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
  * MA 02110-1301, USA.
  */
+
+// GPIO setup macros. Always use INP_GPIO(x) before using OUT_GPIO(x)
+#define INP_GPIO(g) *(gpio+((g)/10)) &= ~(7<<(((g)%10)*3))	// Set GPIO as input.
+#define OUT_GPIO(g) *(gpio+((g)/10)) |=  (1<<(((g)%10)*3))	// Set GPIO as output.
+
+#define GPIO_SET *(gpio + 7)	// Sets bits which are 1 and ignores bits which are 0.
+#define GPIO_CLR *(gpio + 10)	// Clears bits which are 1 and ignores bits which are 0.
+
+void gpio_set(unsigned int);
+void gpio_clear(unsigned int);
+void gpio_input(unsigned int);
+void gpio_output(unsigned int);
+unsigned char gpio_read(unsigned int);
+unsigned int gpio_read_all(void);
